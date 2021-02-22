68fe7cbfe1793119f4f334891891d74ded280fa4
Manuel <manudous@gmail.com>
2021-02-22 22:51:45 +0100

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import {Characters} from './character/character.container'
+import React from 'react';
+
+
+const App: React.FunctionComponent = () => {
+  return <Characters />;
+};
+
+
+
+export default App;
